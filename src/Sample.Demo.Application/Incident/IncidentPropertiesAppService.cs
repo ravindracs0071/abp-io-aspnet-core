08baade1c7e75d4635d7b84895e28f58b6d0453c
ravindracs0071 <ravindracs0071@gmail.com>
2021-03-21 18:35:35 +0530

    added migration

--- src/Sample.Demo.Application/Incident/IncidentPropertiesAppService.cs
+++ src/Sample.Demo.Application/Incident/IncidentPropertiesAppService.cs
@@ -15,27 +15,95 @@ namespace Sample.Demo.Incident
     public class IncidentPropertiesAppService : DemoAppService
     {
         private readonly ICurrentUser _currentUser;
-
-        public IncidentPropertiesAppService(ICurrentUser currentUser)
+        protected PropertySetting.IPropertySettingManager PropertySettingManager { get; }
+        public IncidentPropertiesAppService(ICurrentUser currentUser, PropertySetting.IPropertySettingManager propertySettingManager)
         {
             _currentUser = currentUser;
+            PropertySettingManager = propertySettingManager;
         }
 
-        public Dictionary<string, string> Get()
+        public virtual async Task<dynamic> GetAsync()
         {
-            Dictionary<string, string> config = new Dictionary<string, string>();
-            PropertyInfo[] propertyInfos;
-            propertyInfos = typeof(CreateIncidentDetailDto).GetProperties();
-            foreach (var property in propertyInfos)
+
+            return new 
             {
-                config.Add(property.Name, "");
-            }
-            return config;
+                IncidentNo = await PropertySettingManager.GetOrNullAsync(PropertySettingNames.Incidents.IncidentNo, Volo.Abp.Settings.GlobalSettingValueProvider.ProviderName, null),
+                //SmtpPort = Convert.ToInt32(await SettingManager.GetOrNullGlobalAsync(EmailSettingNames.Smtp.Port)),
+                //SmtpUserName = await SettingManager.GetOrNullGlobalAsync(EmailSettingNames.Smtp.UserName),
+                //SmtpPassword = await SettingManager.GetOrNullGlobalAsync(EmailSettingNames.Smtp.Password),
+                //SmtpDomain = await SettingManager.GetOrNullGlobalAsync(EmailSettingNames.Smtp.Domain),
+                //SmtpEnableSsl = Convert.ToBoolean(await SettingManager.GetOrNullGlobalAsync(EmailSettingNames.Smtp.EnableSsl)),
+                //SmtpUseDefaultCredentials = Convert.ToBoolean(await SettingManager.GetOrNullGlobalAsync(EmailSettingNames.Smtp.UseDefaultCredentials)),
+                //DefaultFromAddress = await SettingManager.GetOrNullGlobalAsync(EmailSettingNames.DefaultFromAddress),
+                //DefaultFromDisplayName = await SettingManager.GetOrNullGlobalAsync(EmailSettingNames.DefaultFromDisplayName),
+            };
+
+            //Dictionary<string, string> config = new Dictionary<string, string>();
+            //PropertyInfo[] propertyInfos;
+            //propertyInfos = typeof(CreateIncidentDetailDto).GetProperties();
+            //foreach (var property in propertyInfos)
+            //{
+            //    config.Add(property.Name, "");
+            //}
+            //return config;
         }
     }
 
-    public class Props 
+    /// <summary>
+    /// Declares names of the property settings.
+    /// </summary>
+    public static class PropertySettingNames
     {
-        
+        ///// <summary>
+        ///// Abp.<......>
+        ///// </summary>
+        //public const string DefaultFromAddress = "Abp.<....>";
+
+        /// <summary>
+        /// Incidents settings.
+        /// </summary>
+        public static class Incidents
+        {
+            /// <summary>
+            /// Abp.Incidents.IncidentNo
+            /// </summary>
+            public const string IncidentNo = "Abp.Incidents.IncidentNo";
+
+            /// <summary>
+            /// Abp.Incidents.IncidentDescr
+            /// </summary>
+            public const string IncidentDescription = "Abp.Incidents.IncidentDescr";
+
+            /// <summary>
+            /// Abp.Incidents.IncidentType
+            /// </summary>
+            public const string IncidentType = "Abp.Incidents.IncidentType";
+
+            /// <summary>
+            /// Abp.Incidents.OccurenceDate
+            /// </summary>
+            public const string OccurenceDate = "Abp.Incidents.OccurenceDate";
+
+            /// <summary>
+            /// Abp.Incidents.ReportTo
+            /// </summary>
+            public const string ReportTo = "Abp.Incidents.ReportTo";
+
+            /// <summary>
+            /// Abp.Incidents.Status
+            /// </summary>
+            public const string Status = "Abp.Incidents.Status";
+
+            /// <summary>
+            /// Reviews settings.
+            /// </summary>
+            public static class Reviews
+            {
+                /// <summary>
+                /// Abp.Incidents.Status
+                /// </summary>
+                public const string IncidentStatus = "Abp.Incidents.Reviews.IncidentStatus";
+            }
+        }
     }
 }
---
+++ src/Sample.Demo.EntityFrameworkCore.DbMigrations/Migrations/20210321125746_properties-tables-settings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sample.Demo.Migrations
+{
+    public partial class propertiestablessettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppPropertySettings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProviderName = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: true),
+                    ProviderKey = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: true),
+                    Visible = table.Column<bool>(type: "bit", nullable: false),
+                    RequiredRegEx = table.Column<bool>(type: "bit", nullable: false),
+                    RegExRule = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppPropertySettings", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppPropertySettings_Name_ProviderName_ProviderKey",
+                table: "AppPropertySettings",
+                columns: new[] { "Name", "ProviderName", "ProviderKey" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppPropertySettings");
+        }
+    }
+}
